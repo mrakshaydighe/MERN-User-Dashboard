e4e2edeb270ce3dc4312589f926a9e72ab8f3503
mrakshaydighe <125887016+mrakshaydighe@users.noreply.github.com>
2024-06-15 17:20:31 +0530

    small changes in backend

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,4 @@
-import userModel from "../models/userModel";
-
-
+import userModel from "../models/userModel.js";
 
 export const create = async (req, res) => {
     try {
@@ -24,33 +22,40 @@ export const getAllUsers = async(req,res)=>{
     try {
         const users = await userModel.find()
         if (!users){
-            return res.status(404).json({msg:`users not founf`})
+            return res.status(404).json({msg:`users not founf`});
         }        
-        res.status(200).json(users)
+        res.status(200).json(users);
     } catch (error) {
-        res.status(500).json({error:error})
+        res.status(500).json({error:error});
     }
 }
 
 export const getUser = async (req,res)=>{
     try {
-        const _id = req.params.id
-        const user = await userModel.findById({_id})
+        const _id = req.params.id;
+        const user = await userModel.findById({_id});
 
         if(!user){
-            return res.status(404).json({msg:`user not found`})
+            return res.status(404).json({msg:`user not found`});
         }
 
-        res.status(200).json(user)
+        res.status(200).json(user);
     } catch (error) {
-        res.status(500).json({error:error})
+        res.status(500).json({error:error});
     }
 }
 
 export const updateUser = async(req,res)=>{
     try {
-        const _id = req.params.id
-        const user = await userModel.findByIdAndUpdate()
+        const _id = req.params.id;
+        const userExits = await userModel.findByIdAndUpdate();
+
+        if(!userExits){
+          return res.status(401).json({msg:`User not found`});
+        }
+
+        const updatedData = userExits.findByIdAndUpdate(_id,req.body,{new:true});
+        res.send(200).json(updatedData);
     } catch (error) {
         res.status(500).json({error:error})
     }
@@ -64,7 +69,6 @@ export const deletUser = async (req, res) => {
       if (!user) {
         return res.status(404).json({ msg: 'Cannot delete user. User not found.' });
       }
-  
       return res.status(200).json({ msg: 'User deleted successfully', user });
     } catch (error) {
       return res.status(500).json({ error: error.message });
--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { create ,getAllUsers,getUser,deletUser } from "../controllers/userController";
+import { create, deletUser, getAllUsers, getUser, updateUser } from "../controllers/userController.js";
+
 
 
 const route = express.Router()
@@ -8,6 +9,7 @@ const route = express.Router()
 route.post("/create",create);
 route.get("/users",getAllUsers);
 route.get("/users/:id",getUser);
+route.put("/users/:id",updateUser)
 route.delete("/users/:id",deletUser);
 
 export default route;
